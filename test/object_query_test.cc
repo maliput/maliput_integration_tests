3cb319c890ab8d38173694d446a49dc22ed2e243
Franco Cipollone <franco.c@ekumenlabs.com>
2022-08-07 20:24:59 -0300

    Fixes format
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- test/object_query_test.cc
+++ test/object_query_test.cc
@@ -183,16 +183,14 @@ TEST_P(SimpleObjectQueryFindLanesTest, FindOverlappingLanes) {
       kId,
       {},
       std::make_unique<math::BoundingBox>(results_.box_position, results_.box_size, results_.box_orientation,
-                                            kTolerance)};
-  const auto intersected_lanes =
-      dut_->FindOverlappingLanesIn(&object, maliput::math::OverlappingType::kIntersected);
+                                          kTolerance)};
+  const auto intersected_lanes = dut_->FindOverlappingLanesIn(&object, maliput::math::OverlappingType::kIntersected);
   EXPECT_EQ(results_.expected_intersected_lane_ids.size(), intersected_lanes.size());
   for (const auto lane : intersected_lanes) {
     EXPECT_FOUND_IN(lane->id(), results_.expected_intersected_lane_ids);
   }
 
-  const auto disjointed_lanes =
-      dut_->FindOverlappingLanesIn(&object, maliput::math::OverlappingType::kDisjointed);
+  const auto disjointed_lanes = dut_->FindOverlappingLanesIn(&object, maliput::math::OverlappingType::kDisjointed);
   EXPECT_EQ(results_.expected_disjointed_lane_ids.size(), disjointed_lanes.size());
   for (const auto lane : disjointed_lanes) {
     EXPECT_FOUND_IN(lane->id(), results_.expected_disjointed_lane_ids);
@@ -257,12 +255,12 @@ TEST_P(SimpleObjectQueryRouteTest, Route) {
       kId,
       {},
       std::make_unique<math::BoundingBox>(results_.origin.position, results_.origin.size, results_.origin.orientation,
-                                            kTolerance)};
+                                          kTolerance)};
   const object::api::Object<maliput::math::Vector3> target_obj{
       kId,
       {},
       std::make_unique<math::BoundingBox>(results_.target.position, results_.target.size, results_.target.orientation,
-                                            kTolerance)};
+                                          kTolerance)};
 
   const auto lane_s_route = dut_->Route(&origin_obj, &target_obj);
   ASSERT_TRUE(lane_s_route.has_value());
